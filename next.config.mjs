f4a2363e538c825f0a3cbb2978a038e2073b0ae3
AMV Vault <recordingsabantu@gmail.com>
2026-03-01 00:11:50 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,3 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    CUSTOM_ENV_VARIABLE: process.env.CUSTOM_ENV_VARIABLE, // You can define environment variables here
-  },
-  images: {
-    domains: ['example.com'], // Allowed image domains
-  },
-  async redirects() {
-    return [
-      {
-        source: '/old-route',
-        destination: '/new-route',
-        permanent: true,
-      },
-    ];
-  },
-};
-
-export default nextConfig;
+const nextConfig = {};
+module.exports = nextConfig;
