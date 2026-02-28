ce88b1515bc482b789aaf1dfeeba931c30cce0bb
AMV Vault <recordingsabantu@gmail.com>
2026-02-28 16:50:39 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,1 +1,19 @@
+import type { Config } from "tailwindcss";
 
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'vault-gold': '#C5A059',
+        'vault-dark': '#0F0908',
+        'vault-brown': '#1E1412',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
