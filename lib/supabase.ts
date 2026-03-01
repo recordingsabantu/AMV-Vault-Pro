575b07d966460ce467f3ccac099c6217cbad4648
AMV Vault <recordingsabantu@gmail.com>
2026-03-01 14:06:40 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+// These use the keys you just added to Netlify
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL as string
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(url, key)
+export default supabase
