c3e364ed240980dc1dcd399f601b1166bb6501c4
AMV Vault <recordingsabantu@gmail.com>
2026-03-02 14:39:19 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,3 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
-
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect();
+export default clerkMiddleware((auth, req) => {
+  // This line tells Clerk: "Don't protect any pages for now"
 });
-
-export const config = {
-  matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
-  ],
-};
