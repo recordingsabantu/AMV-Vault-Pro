e792ee2dabee7951f4492725ae2020e39e1a9424
AMV Vault <recordingsabantu@gmail.com>
2026-03-02 14:54:01 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
+// By leaving this empty, Clerk will not protect ANY routes by default.
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
+    // This allows all pages to load without checking for a user
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
