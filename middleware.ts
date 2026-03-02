d058ef3a4d5ff8b4a219fbf344ec48a66035e53c
AMV Vault <recordingsabantu@gmail.com>
2026-03-02 16:46:05 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// This makes all routes public by default
-export default clerkMiddleware();
+// This empty middleware will stop the "Edge Function" from crashing
+export default clerkMiddleware(() => {
+  return; 
+});
 
 export const config = {
   matcher: [
-    // This regex ensures the middleware doesn't interfere with static files
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
